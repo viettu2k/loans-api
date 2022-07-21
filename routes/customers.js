1f95844acc30ca8c191a7459bde14bde9ce03df0
viettu2k <doanviettu2k@gmail.com>
2022-07-21 15:55:08 +0700

    import model

--- routes/customers.js
+++ routes/customers.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+const customerModel = require("../models/customers.model");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+const invoiceModel = require("../models/invoices.model");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
